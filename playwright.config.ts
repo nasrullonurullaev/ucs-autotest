b41a42ccae59da00b3e680168644c90cc1fe2eac
Nasrullo Nurullaev <61620246+nasrullonurullaev@users.noreply.github.com>
2026-03-16 22:27:11 +0500

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  timeout: 0,
+
+  retries: process.env.CI ? 2 : 0,
+
+  workers: process.env.CI ? 1 : undefined,
+
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+  ],
+
+  use: {
+    headless: true,
+
+    ignoreHTTPSErrors: true,
+
+    actionTimeout: 0,
+    navigationTimeout: 0,
+
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure'
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        browserName: 'chromium'
+      }
+    }
+  ]
+});
